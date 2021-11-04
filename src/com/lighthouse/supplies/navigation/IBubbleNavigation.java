b9ba1e053f87219d982c2bf2f6f4c3c23a031881
Stealth1226 <rishawniyer06@gmail.com>
2021-11-04 21:02:47 +0530

    Initial cleanup for Supplies

---
+++ src/com/lighthouse/supplies/navigation/IBubbleNavigation.java
@@ -0,0 +1,15 @@
+package com.lighthouse.supplies.navigation;
+
+import android.graphics.Typeface;
+import com.lighthouse.supplies.navigation.BubbleNavigationChangeListener;
+
+@SuppressWarnings("unused")
+public interface IBubbleNavigation {
+    void setNavigationChangeListener(BubbleNavigationChangeListener navigationChangeListener);
+
+    void setTypeface(Typeface typeface);
+
+    int getCurrentActiveItemPosition();
+
+    void setCurrentActiveItem(int position);
+}
